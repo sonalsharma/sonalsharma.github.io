abcb534163e255836298171cbed4d3ad48a7f9c9
sonalsharma <sonalsharma@live.ca>
2014-11-17 13:41:56 -0500

    fixing nav

--- js/main.js
+++ js/main.js
@@ -1,35 +1,49 @@
-var $win = $(window);
-var $sun = $('.sun');
-var $sunSection = $('.sun-section');
-var $dipperSection = $('.dipper-section');
-var $dipper = $('.dipper');
-var $shuttleSection = $('.shuttle-section');
-var $shuttle = $('.shuttle');
-var $shipSection = $('.ship-section');
-var $ship = $('.spaceship');
-var $ship3 = $('.ship-3')
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "menu" link is shown
+	var offset = 300;
 
-$win.on('scroll', function () {
-	var scrollPos = $win.scrollTop();
-	
-	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+	var navigationContainer = $('#cd-nav'),
+		mainNavigation = navigationContainer.find('#cd-main-nav ul');
 
-	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px')
+	//hide or show the "menu" link
+	checkMenu();
+	$(window).scroll(function(){
+		checkMenu();
+	});
 
+	//open or close the menu clicking on the bottom "menu" link
+	$('.cd-nav-trigger').on('click', function(){
+		$(this).toggleClass('menu-is-open');
+		//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
+		mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
+
+	});
+
+	function checkMenu() {
+		if( $(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
+			navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+				mainNavigation.addClass('has-transitions');
+			});
+		} else if ($(window).scrollTop() <= offset) {
+			//check if the menu is open when scrolling up
+			if( mainNavigation.hasClass('is-visible')  && !$('html').hasClass('no-csstransitions') ) {
+				//close the menu with animation
+				mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					//wait for the menu to be closed and do the rest
+					mainNavigation.removeClass('is-visible is-hidden has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+				});
+			//check if the menu is open when scrolling up - fallback if transitions are not supported
+			} else if( mainNavigation.hasClass('is-visible')  && $('html').hasClass('no-csstransitions') ) {
+					mainNavigation.removeClass('is-visible has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+			//scrolling up with menu closed
+			} else {
+				navigationContainer.removeClass('is-fixed');
+				mainNavigation.removeClass('has-transitions');
+			}
+		} 
+	}
 });
-
-$dipperSection.waypoint(function () {
-	$dipper.addClass('js-dipper-fade');
-}, { offset:'50%' });
-
-$shuttleSection.waypoint(function () {
-	$shuttle.addClass('js-shuttle-fade');
-}, {offset:'50%'});
-
-$shipSection.waypoint(function () {
-	$ship.addClass('js-ship-fade');
-}, {offset:'50%'} );
-
-$shipSection.waypoint(function () {
-	$ship3.addClass('ship3');
-}, {offset:'50%'} );
