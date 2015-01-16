cbc1124388a183af40263e20440579d9ea7067bb
sonalsharma <sonalsharma@live.ca>
2015-01-15 22:46:52 -0500

    added new nav, quote,

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Show nav on scroll */
+
 var $win = $(window);
 var $iLove = $('.ilove');
 var $icons = $('.icons img');
@@ -6,7 +8,7 @@ var iLoveOffset = $iLove.offset();
 	$(window).scroll(function() {    
 	    var scroll = $(window).scrollTop();
 
-	    if (scroll >= 30) {
+	    if (scroll >= 20) {
 	        $(".nav").addClass("scrolling");
 	    } else {
 	        $(".nav").removeClass("scrolling");
@@ -29,49 +31,22 @@ var iLoveOffset = $iLove.offset();
 
 /*----Smooth scroll for in page links---*/
 
-$(function(){
-    var target, scroll;
+/*$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
 
-    $("a[href*=#]:not([href=#])").on("click", function(e) {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            target = $(this.hash);
-            target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
+        var target = this.hash;
+        var $target = $(target);
 
-            if (target.length) {
-                if (typeof document.body.style.transitionProperty === 'string') {
-                    e.preventDefault();
-                  
-                    var avail = $(document).height() - $(window).height();
-
-                    scroll = target.offset().top;
-                  
-                    if (scroll > avail) {
-                        scroll = avail;
-                    }
-
-                    $("html").css({
-                        "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
-                        "transition" : "1s ease-in-out"
-                    }).data("transitioning", true);
-                } else {
-                    $("html, body").animate({
-                        scrollTop: scroll
-                    }, 1000);
-                    return;
-                }
-            }
-        }
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'linear', function () {
+        });
     });
+});*/
 
-    $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
-        if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
-            $(this).removeAttr("style").data("transitioning", false);
-            $("html, body").scrollTop(scroll);
-            return;
-        }
-    });
-});
 
+/* Icon animations */
 
 jQuery(document).ready(function() {
 
@@ -104,9 +79,14 @@ jQuery(document).ready(function() {
         classToAdd: 'logovis animated fadeInLeft',
         offset: 100
        });
+
+     jQuery('.quote').addClass("hideme").viewportChecker({
+        classToAdd: 'visible animated fadeInLeft',
+        offset: 100
+       });
 });
 
-
+/* Scroll to top for mobile */ 
 $(function(){
  
     $(document).on( 'scroll', function(){
@@ -128,3 +108,51 @@ function scrollToTop() {
     offsetTop = offset.top;
     $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
 }
+
+/* ---------------------------------------------------------- */
+
+
+
+/*$(document).ready(function() {
+    
+    // Portfolio section 
+    
+    // Toggle a portfolio item
+    $('.portfolio-work a').click(function() {
+        $('.portfolio-details .portfolio-item').hide();
+        
+        var itemId = $(this).attr('href');
+        $(itemId).show();
+        
+        if ($('.portfolio-details').css('display') == 'none') {
+            $('.portfolio-details').slideDown();
+        }
+        
+       $.smoothScroll({
+          scrollTarget: '#portfolio-details',
+          offset: -300
+        });
+
+        return false;
+
+        
+    });
+
+    
+    // Close portfolio
+    $('#close-portfolio').click(function() {
+        $('.portfolio-details').slideUp();
+        return false;
+    });
+    // END - Portfolio section  
+    
+});
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});*/
+
+
+
